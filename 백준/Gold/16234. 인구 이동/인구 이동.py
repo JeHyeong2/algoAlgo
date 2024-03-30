f9daf275a89130f3aa209c389f30b0e96eb401e5
Shin_bro <122584189+JeHyeong2@users.noreply.github.com>
2024-03-30 23:02:01 +0900

    [Gold IV] Title: 인구 이동, Time: 1512 ms, Memory: 213752 KB -BaekjoonHub

---
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -0,0 +1,58 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+
+def move_country():
+    global end,count
+    stoped = True
+    visited = [[-1]*N for _ in range(N)]
+    weight = [[0]*2 for _ in range((N+1)*(N+1)*4)]
+    q = deque()
+    q.append((0,0,0))
+    numcount = 0
+
+    while q:
+        s1,s2,country_num = q.popleft()
+
+        if visited[s1][s2] != -1:
+            continue
+        weight[country_num][0] += countries[s1][s2]
+        weight[country_num][1] +=1
+        visited[s1][s2] = country_num
+        for k in range(4):
+            ni = s1 + di[k]
+            nj = s2 + dj[k]
+            if 0 <= ni < N and 0 <= nj < N:
+                if L <= abs(countries[s1][s2] - countries[ni][nj]) <= R:
+                    q.appendleft((ni,nj,country_num))
+                else:
+                    if visited[ni][nj] == -1:
+                        numcount +=1
+                        q.append((ni,nj,numcount))
+    for i in range(N):
+        for j in range(N):
+            if weight[visited[i][j]][1] >1:
+                countries[i][j] = weight[visited[i][j]][0] // weight[visited[i][j]][1]
+                stoped = False
+    if stoped:
+        print(count)
+        end = False
+    else:
+        count +=1
+    return
+
+
+
+
+
+N,L,R = map(int,input().split())
+
+di=[-1,0,1,0]
+dj=[0,1,0,-1]
+end= True
+count = 0
+countries = [list(map(int,input().split())) for _ in range(N)]
+while end:
+    move_country()
