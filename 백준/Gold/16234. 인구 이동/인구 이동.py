b656232f24d8ef8f5c10fe348d1a013fd7fb3f07
Shin_bro <122584189+JeHyeong2@users.noreply.github.com>
2024-03-30 23:35:10 +0900

    [Gold IV] Title: 인구 이동, Time: 1464 ms, Memory: 180848 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -4,11 +4,50 @@ import sys
 input = sys.stdin.readline
 
 
+# def move_country():
+#     global end,count
+#     stoped = True
+#     visited = [[-1]*N for _ in range(N)]
+#     weight = [[0]*2 for _ in range(N*N*4)]
+#     q = deque()
+#     q.append((0,0,0))
+#     numcount = 0
+#
+#
+#     while q:
+#         s1,s2,country_num = q.popleft()
+#
+#         if visited[s1][s2] != -1:
+#             continue
+#         weight[country_num][0] += countries[s1][s2]
+#         weight[country_num][1] +=1
+#         visited[s1][s2] = country_num
+#         for k in range(4):
+#             ni = s1 + di[k]
+#             nj = s2 + dj[k]
+#             if 0 <= ni < N and 0 <= nj < N:
+#                 if L <= abs(countries[s1][s2] - countries[ni][nj]) <= R:
+#                     q.appendleft((ni,nj,country_num))
+#                 else:
+#                     if visited[ni][nj] == -1:
+#                         numcount += 1
+#                         q.append((ni,nj,numcount))
+#     for i in range(N):
+#         for j in range(N):
+#             if weight[visited[i][j]][1] > 1:
+#                 countries[i][j] = weight[visited[i][j]][0] // weight[visited[i][j]][1]
+#                 stoped = False
+#     if stoped:
+#         print(count)
+#         end = False
+#     else:
+#         count +=1
+#     return
 def move_country():
     global end,count
     stoped = True
     visited = [[-1]*N for _ in range(N)]
-    weight = [[0]*2 for _ in range((N)*(N)*4)]
+    weight = {}
     q = deque()
     q.append((0,0,0))
     numcount = 0
@@ -19,8 +58,13 @@ def move_country():
 
         if visited[s1][s2] != -1:
             continue
-        weight[country_num][0] += countries[s1][s2]
-        weight[country_num][1] +=1
+        if country_num not in weight:
+            weight[country_num] = []
+            weight[country_num].append(countries[s1][s2])
+            weight[country_num].append(1)
+        else:
+            weight[country_num][0] += countries[s1][s2]
+            weight[country_num][1] +=1
         visited[s1][s2] = country_num
         for k in range(4):
             ni = s1 + di[k]
@@ -43,6 +87,8 @@ def move_country():
     else:
         count +=1
     return
+
+
 
 
 
