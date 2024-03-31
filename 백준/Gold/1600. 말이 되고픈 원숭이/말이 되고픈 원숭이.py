a60d94a95b7cc4a6bf2e7cc6fee6dff6648deda9
Shin_bro <122584189+JeHyeong2@users.noreply.github.com>
2024-04-01 04:09:23 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 596 ms, Memory: 149224 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
@@ -1,49 +1,55 @@
 from collections import deque
 import sys
+
 input = sys.stdin.readline
- 
-K = int(input())
-W, H = map(int, input().split()) # 열, 행
-board = [list(map(int, input().split())) for _ in range(H)]
- 
-# 말처럼 이동하는 경우
-dx_knight = [-2, -1, 1, 2, 2, 1, -1, -2]
-dy_knight = [1, 2, 2, 1, -1, -2, -2, -1]
- 
-# 원숭이 처럼 상하좌우로 이동하는 경우
-dx_monkey = [0, 0, -1, 1]
-dy_monkey = [1, -1, 0, 0]
- 
-def bfs():
-    visited = [[[False] * (K+1) for _ in range(W)] for _ in range(H)]
-    visited[0][0][K] = True
-    queue = deque([(0, 0, K, 0)])
- 
-    while queue:
-        x, y, k, move = queue.popleft()
- 
-        # 목표 지점 도달
-        if x == H-1 and y == W-1:
-            return move
- 
-        # 말처럼 이동하는 경우 - k(남은 움직일 수 있는 이동 횟수)가 1 이상인지 확인
-        if k:
-            for i in range(8):
-                nx = x + dx_knight[i]
-                ny = y + dy_knight[i]
- 
-                if 0 <= nx < H and 0 <= ny < W and not visited[nx][ny][k-1] and board[nx][ny] != 1:
-                    visited[nx][ny][k-1] = True
-                    queue.append((nx, ny, k-1, move+1)) # 말처럼 움직인 횟수 1 차감, 이동 횟수 1 증감
- 
-        # 원숭이 처럼 상하좌우로 이동하는 경우
-        for i in range(4):
-            nx = x + dx_monkey[i]
-            ny = y + dy_monkey[i]
- 
-            if 0 <= nx < H and 0 <= ny < W and not visited[nx][ny][k] and board[nx][ny] != 1:
-                visited[nx][ny][k] = True
-                queue.append((nx, ny, k, move+1))
+
+def move():
+    global _min
+    weight_field = [[[0]*(K+1) for _ in range(N)] for _ in range(M)]
+    q = deque()
+    q.append((0,0,0))
+    weight_field[0][0][0]=1
+
+    while q:
+        s1,s2,horse_count = q.popleft()
+
+        if (s1,s2) == (M-1,N-1):
+            return weight_field[s1][s2][horse_count] -1 
+
+        if horse_count < K:
+            for w in range(8):
+                hi = s1 + horse_x[w]
+                hj = s2 + horse_y[w]
+                if 0 <= hi < M and 0 <= hj < N and field[hi][hj] != 1:
+                    if weight_field[hi][hj][horse_count+1] == 0:
+                        weight_field[hi][hj][horse_count+1] = weight_field[s1][s2][horse_count]+1
+                        q.append((hi, hj, horse_count+1))
+
+        for k in range(4):
+            ni = s1 + di[k]
+            nj = s2 + dj[k]
+            if 0 <= ni < M and 0 <= nj < N and field[ni][nj] != 1:
+                if weight_field[ni][nj][horse_count] == 0:
+                    weight_field[ni][nj][horse_count] = weight_field[s1][s2][horse_count]+1
+                    q.append((ni,nj,horse_count))
+
     return -1
- 
-print(bfs())
+
+
+
+
+
+
+K = int(input())
+N,M = map(int,input().split())
+field = [list(map(int,input().split())) for _ in range(M)]
+
+
+horse_x = [-1,-2,-1,-2,1,2,1,2]
+horse_y = [-2,-1,2,1,-2,-1,2,1]
+
+di = [-1,0,1,0]
+dj = [0,1,0,-1]
+
+
+print(move())
