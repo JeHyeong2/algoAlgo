4177b9da63e23ad455dccb370554fa27de4e25dc
Shin_bro <122584189+JeHyeong2@users.noreply.github.com>
2024-04-12 02:34:51 +0900

    [Silver I] Title: 회전 초밥, Time: 132 ms, Memory: 112124 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -1,18 +1,53 @@
+import sys
+input = sys.stdin.readline
+
 N, D, K, C = map(int,input().split())
 
 dish = []
-_max = 0
 for i in range(N):
     dish.append(int(input()))
+counting_list = [0]*(D+1)
+counting_list[C] = 1
+count =1
 
 dish.extend(dish[:K-1])
 
-for i in range(N):
-    can_eat = set(dish[i:i+K])
-    can_eat.add(C)
-    if len(can_eat) > _max:
-        _max = len(can_eat)
-    if _max == K+1:
-        break
+for i in range(K):
+    if counting_list[dish[i]] == 0:
+        counting_list[dish[i]] = 1
+        count += 1
+    else:
+        counting_list[dish[i]] += 1
+
+_max = count
+
+for i in range(1,N):
+
+    counting_list[dish[i-1]] -= 1
+
+    if counting_list[dish[i-1]] == 0:
+        count -= 1
+
+    if counting_list[dish[i+K-1]] == 0:
+        counting_list[dish[i+K-1]] += 1
+        count +=1
+    else:
+        counting_list[dish[i + K - 1]] += 1
+    _max = max(_max,count)
+
+
+
+# for i in range(N):
+#     dish.append(int(input()))
+#
+# dish.extend(dish[:K-1])
+#
+# for i in range(N):
+#     can_eat = set(dish[i:i+K])
+#     can_eat.add(C)
+#     if len(can_eat) > _max:
+#         _max = len(can_eat)
+#     if _max == K+1:
+#         break
 
 print(_max)
