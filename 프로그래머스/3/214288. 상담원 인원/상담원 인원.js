c00d7d611490266a191c56e916b1ee6b3accf55d
Shin_bro <122584189+JeHyeong2@users.noreply.github.com>
2024-05-24 18:20:46 +0900

    [level 3] Title: 상담원 인원, Time: 6.77 ms, Memory: 38.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/214288. 상담원 인원/상담원 인원.js
@@ -0,0 +1,64 @@
+function solution(k, n, reqs) {
+    let answer = 0;
+    const client = new Array(k+1).fill(0).map(()=>[])     
+    
+   
+    const wasteCheck = (arr,man) => {
+      let time = 0
+      const table = new Array(man).fill(0).map(()=>0)
+      let pointer = 0
+      const f1 = () =>{
+        let tmp = table[0] - arr[pointer][0]
+        table.shift()
+        table.push(arr[pointer][0] + arr[pointer][1])
+        pointer += 1
+        table.sort((a,b)=>(a-b))
+      }
+      const f2 = ()=>{
+        let tmp = table[0] - arr[pointer][0]
+        time += table[0] - arr[pointer][0]                 
+        table.shift()
+        table.push(arr[pointer][0] + arr[pointer][1]+tmp)
+         pointer += 1
+         table.sort((a,b)=>a-b)
+      }
+      
+      
+     for (let i = 0 ; i < arr.length ; i++){
+       table[0] <= arr[pointer][0] ? f1() : f2()
+     }
+    return time
+      
+     }
+   
+    
+
+  for(let i = 0 ; i < reqs.length; i++){
+      client[reqs[i][2]].push(reqs[i])
+    }
+  const mentoNum = new Array(k+1).fill(1)
+  let mento = n - k
+  while(mento){
+    let idx = 0
+    let _max = 0
+    for(let i = 1; i < client.length; i++){
+      const pre = wasteCheck(client[i],mentoNum[i])
+      const plus =wasteCheck(client[i],(mentoNum[i]+1))
+      if(pre - plus > _max){
+        _max = pre - plus
+        idx = i
+      }
+    }
+    mentoNum[idx]+=1
+    mento -=1
+  }
+
+  for(let i = 1; i < client.length; i ++){
+    let anstime = wasteCheck(client[i],mentoNum[i])
+    answer += anstime
+  }
+
+
+  
+    return answer;
+}
