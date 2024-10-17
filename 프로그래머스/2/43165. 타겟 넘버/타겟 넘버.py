6d1efbc14673cfca8e2bfc6dc214ff8c52239122
Shin_bro <122584189+JeHyeong2@users.noreply.github.com>
2024-10-18 02:47:16 +0900

    [level 2] Title: 타겟 넘버, Time: 446.90 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+def solution(numbers, target):
+    
+    answer = 0
+    length = len(numbers)
+    
+
+    def make_deal():
+        ans = 0
+        q = [(0,0)] 
+        
+        while q:
+            now,_sum = q.pop()
+            if now == length:
+                if _sum == target:
+                    ans +=1
+                    continue
+                else:
+                    continue
+            
+            plus = _sum + numbers[now] 
+            minus = _sum + -numbers[now]
+            q.append((now+1, plus))
+            q.append((now+1, minus))
+            
+        return ans
+   
+            
+    
+    return make_deal()
